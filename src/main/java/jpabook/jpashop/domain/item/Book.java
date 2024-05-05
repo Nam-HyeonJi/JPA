030c28505297cb9284e3c31c6338b5c390e15e2f
DESKTOP-P581UF8\primo <a01086905329@gmail.com>
2024-05-05 19:33:28 +0900

    bootstrap 추가 화면단 회원, 상품 추가

--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jpabook.jpashop.Controller.BookForm;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,4 +13,15 @@ public class Book extends Item{
 
     private String author;
     private String isbn;
+
+    //생성메서드
+    public static Book createBook(BookForm form) {
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+        return book;
+    }
 }
